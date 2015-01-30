dc3cc8eb37c7f2db729ba339a9be14f3666d48eb
Andrew Fecheyr <andrew@bedesign.be>
2015-01-30 11:49:15 +0100

    working validator

--- lib/email_domain_inclusion.rb
+++ lib/email_domain_inclusion.rb
@@ -1,5 +1,15 @@
 require "email_domain_inclusion/version"
 
-module EmailDomainInclusion
-  # Your code goes here...
+# A custom validator that checks that the email's domain is included in the specified domains
+# Usage: validates :email, email_domain: {allowed_domains: ->() { AllowedEmailDomains.pluck(:domain) } }
+# +allowed_domains+ can be an array of strings, or a Proc that returns an array of strings
+class EmailDomainInclusionValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    domains = options[:allowed_domains]
+    domains = domains.call if domains.respond_to?(:call)
+    domain = Mail::Address.new(value).try(:domain).try(:downcase)
+    unless domains.include?(domain)
+      record.errors[attribute] << options[:message] || "domain_not_in_list"
+    end
+  end
 end
